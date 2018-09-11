23d38ded6046bdc77ecf25c0012500a0715c2aee
Dalton <daltonapolo11@gmail.com>
2018-09-11 20:11:43 -0300

    Adicionando modelos

---
+++ events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    priorities_list =(('0', 'Sem Prioridade'), ('1', 'Normal'), ('2', 'Urgente'), ('3', 'Muito Urgente'), ('4', 'Ultra Mega Hiper Urgente'))
+    date = models.DateField()
+    event = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=priorities_list)
+    def __str__(self):
+        return self.event
