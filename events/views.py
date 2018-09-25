9d8740de4670afc0ca6b0b1b63a9f7fcdf2d2c88
Dalton <daltonapolo11@gmail.com>
2018-09-25 19:28:27 -0300

    Adicionando funçoes

--- events/views.py
+++ events/views.py
@@ -5,8 +5,18 @@ from django.core.paginator import Paginator, InvalidPage
 from django.http import HttpResponse
 from django.views.defaults import bad_request, server_error
 from datetime import datetime, timedelta
+from .forms import EventForm, CommentForm
+m
+from datetime import datetime, timedelta
 
 ITEMS_PER_PAGE = 5
+
+def split_date(string_date):
+    """Transforma a data em YYY-MM-DD em uma tupla sde três valores
+    para ultilizar na visão de eventos de um determinando dia."""
+    for value in string_date.split('-'):
+        yield int(value)
+
 
 # Create your views here.
 def index(request):
@@ -59,3 +69,65 @@ def day(request, year:int, month:int, day:int):
     return render(request, 'day.html', context)
 
 
+def delete(request, id: int):
+    """ Apaga um evento especifico, se o evento não existir resultará em erro 404, se algo 
+    errado ocorrer retornará a página de erro."""
+    event = get_object_or_404(Event, id=id)
+    (year, month, day) = tuple(
+        split_date('{:%y-%m-%d}'.format(event.date)))
+    if event.delete():
+        return redirect('agenda-events-day', year=year, month=month, day=day)
+    else:
+        return server_error(request, 'ops_500.html')
+
+def edit(request):
+    """Editar o conteúdo de um evento, recebendo os dados enviandos pelo
+    formulário, validando e populando"""
+
+    form = EventForm(request.POST)
+    if form.is_valid():
+        event = get_object_or_404(Event, id=request.POST['id'])
+        event.date = form.cleaned_data['date']
+        event.event = form.cleaned_data('event')
+        event.priority = form.cleaned_data['priority']
+        event.save()
+        (year, month, day) = tuple(
+            split_date('{:%y-%m-%d}'.format(event.date)))
+        return redirect('agensa-events-day', year=year, month=month, day=day)
+    else:
+        return bad_request(request, None, 'ops_400.html')
+
+
+def new(request):
+    """Recebe os dados de um nnovo evento via POST, fa a validação dos dados
+    e ai insere na base de dados"""
+
+    form = EventForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+
+        # uso a data enviada pelo formulário para o redirecionamento.
+        (year, month, day) = tuple(
+        split_date(request.POST['date']))
+        return redirect('agenda-events-day', year=year, month=month, day=day)
+    else:
+        return bad_request(request, None, 'ops_400.html')
+
+def show(request, id: int):
+    """Visualizaç]ao de um determinado evento e de seus comnetários, recebe
+    o 'id' do evento. Caso seja acessado via POST insere um novo comentário."""
+    event = get_object_or_404()(Event, id=id)
+    if bad_request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('agenda-events-show', id=id)
+
+    context = {
+        'event': event,
+        'comments': Comment.objects.filter(event=id).order_by('-commented'),
+        'hide_new_button': Event.priorities_list,
+        'today': localdate(),
+    }
+
+    return render(request, 'show.html', context)
